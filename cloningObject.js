2e301cf7d45ca18a1a6441067375d4839934ccdc
atita <anaguasoft@gmail.com>
2018-07-05 16:12:44 +0300

    Object.assign and changing object properties

---
+++ cloningObject.js
@@ -0,0 +1,26 @@
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      // don't mutate state here or the tests will fail
+     let Obj = Object.assign({},state)
+         Obj.status = 'online'
+     return Obj
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
